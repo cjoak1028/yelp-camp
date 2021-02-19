11d5e852eaa3314374a3c6df9e1ae647a78ea57c
David Kim <cjoak1028@gmail.com>
2021-02-19 11:03:14 -0500

    added basic styles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -18,6 +19,8 @@ db.once("open", () => {
 
 const app = express();
 
+// Change ejs engine
+app.engine('ejs', ejsMate);
 // Set view as ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +30,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // Override method with query string
 app.use(methodOverride('_method'));
+
+
+// HOME
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 // INDEX ROUTE
 app.get('/campgrounds', async (req, res) => {
