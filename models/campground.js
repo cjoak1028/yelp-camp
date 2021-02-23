4e8922a98bc21fbaa0051284299205b64397f9e4
David Kim <cjoak1028@gmail.com>
2021-02-22 19:28:21 -0500

    added reviews model

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const Review = require('./review');
+
 const CampGroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
+})
+
+CampGroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Campground', CampGroundSchema);
